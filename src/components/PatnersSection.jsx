c9b78ae7a73c17f6900258f98891370f28571f6c
RehkMansa <godspowernathaniel25@gmail.com>
2022-07-14 23:05:13 +0100

    added partners image

--- src/components/PatnersSection.jsx
+++ src/components/PatnersSection.jsx
@@ -1,5 +1,18 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 30px;
+  padding: 100px 0;
+`;
 const PartnersSection = () => {
-  return (  );
-}
- 
+  return (
+    <Wrapper>
+      <img src="/images/add" alt="partners" />
+    </Wrapper>
+  );
+};
+
 export default PartnersSection;
